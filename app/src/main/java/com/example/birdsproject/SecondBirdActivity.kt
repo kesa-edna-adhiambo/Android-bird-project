858328c7e8c8461809ec880e55e0a281fea08b89
kesa-edna-adhiambo <edna@gmail.com>
2024-06-11 20:36:06 +0300

    fisrt bird activity

---
+++ app/src/main/java/com/example/birdsproject/SecondBirdActivity.kt
@@ -0,0 +1,11 @@
+package com.example.birdsproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondBirdActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_bird)
+    }
+}

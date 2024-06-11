e3b0a5c1f06ef2e00f146329f0f0e357c485d8e8
kesa-edna-adhiambo <edna@gmail.com>
2024-06-11 23:09:07 +0300

    fisrt bird activity

--- app/src/main/java/com/example/birdsproject/FifthBirdsActivity.kt
+++ app/src/main/java/com/example/birdsproject/FifthBirdsActivity.kt
@@ -3,9 +3,9 @@ package com.example.birdsproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class FifthBirdsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_fifth_birds)
     }
 }
